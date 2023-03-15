5c19c60ad067a1e28ebea18ad59c273228653ab5
Sihle Hlophe <wldrms@outlook.com>
2023-03-15 21:22:03 +0200

    sign up for drivers hyperlinks upated

---
+++ ipharm/js/form-validation.js
@@ -0,0 +1,37 @@
+// function validateForm() {
+//   const email = document.getElementById("Email").value;
+//   const password = document.getElementById("Password").value;
+//   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+//   if (email.trim() === "" || !emailRegex.test(email)) {
+//     alert("Please enter a valid email address.");
+//     return false;
+//   }
+
+//   if (password.trim() === "") {
+//     alert("Please enter your password.");
+//     return false;
+//   }
+
+//   return true;
+// }
+
+// const email = document.getElementById("Email").value;
+// const password = document.getElementById("Password").value;
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+
+//   validateInputs();
+// });
+
+// const setError = (element, message) => {
+//   const inputControl = element.parentElement;
+//   const errorDisplay = inputControl.querySelector('.error');
+// }
+
+// const validateInputs = () => {
+//     const usernameValue = username.value.trim();
+//     const emailValue = email.value.trim();
+
+// };
