246df7c89f6ab3ecd1f5925582465ff51480d0cf
Sihle Hlophe <wldrms@outlook.com>
2023-03-20 23:57:53 +0200

    orders page added

--- ipharm/js/count.js
+++ ipharm/js/count.js
@@ -1,10 +1,17 @@
 let count = 1;
 
-        document.getElementById("decrement").onclick = function() {
-                 count -=1;
-        document.getElementById("quantity").innerHTML = count;
-        }
-        document.getElementById("increment").onclick = function() {
-                 count +=1;
-        document.getElementById("quantity").innerHTML = count;
-        }
+document.getElementById("decrement").onclick = function() {
+    count -=1;
+    document.getElementById("quantity").value = count;
+}
+document.getElementById("increment").onclick = function() {
+    count +=1;
+    document.getElementById("quantity").value = count;
+}
+
+if (count.valueOf < 1){
+    count.valueOf +=1;
+}
+else if (count.valueOf === NaN) {
+    count.valueOf = 1
+}
