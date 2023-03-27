5be780375446f0bf5fcf60d76f40f6be25c4ed50
Sihle Hlophe <wldrms@outlook.com>
2023-03-27 17:53:15 +0200

    validation.js updated

--- ipharm/js/validation.js
+++ ipharm/js/validation.js
@@ -1,12 +1,32 @@
-function validateForm() {
-    const email = document.getElementById("email");
-    const password = document.getElementById("password");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
 
-    if (email.value =="" || password.value ==""){
-        alert("No blank valus allowed");
-        return false;
+function validateForm() {
+    // Checking if email is emppty
+    if (email.value.trim() ===""){
+       onError(email,"Email cannot be empty");
     }
     else {
-        true;
+        onSuccess(email);
     }
+}
+
+document.querySelector("button")
+document.addEventListener("click", (event)=>{
+    event.preventDefault();
+    validateForm();
+});
+
+function onSuccess(input){
+    let parent = email.parentElement;
+        let messageEle = parent.querySelector("small");
+        messageEle.style.visibility = "hidden";
+        messageEle.innerText = "";
+}
+
+function onError(input){
+    let parent = email.parentElement;
+        let messageEle = parent.querySelector("small");
+        messageEle.style.visibility = "visible";
+        messageEle.innerText = "Email cannot be empty";
 }
